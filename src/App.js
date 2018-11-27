a5845b78d7d938ca071148a0ce1d88513e0b6895
Leediyal <lydia14jeba@gmail.com>
2018-11-28 00:39:51 +0530

    Adding Markers and InfoWindows

--- src/App.js
+++ src/App.js
@@ -5,18 +5,62 @@ import FoursquareAPI from './components/FoursquareAPI.js';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      venues: [],
+      markers: [],
+      center: [],
+      zoom: 12
+    }
+  }
+
+  closeMarkers = () => {
+    const markers = this.state.markers.map(marker => {
+      marker.isOpen = false;
+      return marker;
+    });
+    this.setState({markers: Object.assign(this.state.markers, markers)});
+  }
+
+  handleClick = (marker) => {
+    this.closeMarkers();
+    marker.isOpen = true;
+    this.setState({markers: Object.assign(this.state.markers, marker)});
+    const venue = this.state.venues.find(venue => venue.id === marker.id);
+    
+    FoursquareAPI.getVenueDetails(marker.id).then(res => {
+      const newVenue = Object.assign(venue, res.response.venue);
+        this.setState({venues: Object.assign(this.state.venues, newVenue)});
+      console.log(newVenue);
+    });
+  };
+
   componentDidMount(){
     FoursquareAPI.search({
       near: "Nilgiris, India",
       query: "museum"
     }).then(results => {
+      const { venues } = results.response;
+      const { center } = results.response.geocode.feature.geometry;
+      const markers = venues.map(venue => {
+        return {
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          isOpen: false,
+          isVisible: true,
+          id: venue.id
+        };
+      });
+      this.setState({venues, center, markers});
       console.log(results.response.venues)
     });
   }
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map {...this.state}
+        handleClick = {this.handleClick}/>
       </div>
     );
   }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
 
 const MyMapComponent = withScriptjs(withGoogleMap((props =>
     <GoogleMap
         defaultZoom={8}
+        zoom={props.zoom}
         defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        center={props.center}
     >
-        {props.isMarkerShown && 
-        <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+        {props.markers && 
+            props.markers.filter(marker => marker.isVisible)
+            .map((marker, index) => {
+                const venueInfo = props.venues.find(venue => venue.id ===marker.id);
+            return (
+                <Marker 
+                    key={index} 
+                    position={{ lat: marker.lat, lng: marker.lng }}
+                    onClick={() => props.handleClick(marker)}
+                >
+                {marker.isOpen && venueInfo.bestPhoto && (
+                <InfoWindow>
+                    <React.Fragment>
+                        <img src={`${venueInfo.bestPhoto.prefix}200*200${venueInfo.bestPhoto.suffix}`} alt={"Museum's View"}/>
+                            <p><strong>{venueInfo.name}</strong></p>
+                            <p><i>{venueInfo.location.formattedAddress[0]}</i></p>
+                            <p><i>{venueInfo.location.formattedAddress[1]}</i></p>
+                            <p><i>{venueInfo.location.formattedAddress[2]}</i></p>
+                            <p><i>{venueInfo.location.formattedAddress[3]}</i></p>
+                    </React.Fragment>
+                </InfoWindow>
+                )}
+                </Marker>
+            );
+        })}
     </GoogleMap>
 ))
 );
@@ -16,7 +41,7 @@ export default class Map extends Component {
     render() {
         return(
             <MyMapComponent
-                isMarkerShown
+                {...this.props}
                 googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&
                 key=AIzaSyBTi9iAGPvuejPFOjQAs-1JhcFMtDNrd68"
                 loadingElement={<div style={{ height: `100%` }} />}
