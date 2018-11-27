f4571a30a7f9b1c86a6e38b99a88652aceb25389
Leediyal <lydia14jeba@gmail.com>
2018-11-27 20:58:29 +0530

    Getting locations from Foursquare API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map.js';
+import FoursquareAPI from './components/FoursquareAPI.js';
 
 class App extends Component {
+
+  componentDidMount(){
+    FoursquareAPI.search({
+      near: "Nilgiris, India",
+      query: "museum"
+    }).then(results => {
+      console.log(results.response.venues)
+    });
+  }
   render() {
     return (
       <div className="App">
---
+++ src/components/FoursquareAPI.js
@@ -0,0 +1,50 @@
+class Helper {
+    static baseURL() {
+        return "https://api.foursquare.com/v2"
+    }
+    static auth() {
+        const keys = {
+            client_id: "ICN1F4HULSQA42LDTNL04DPF4T2BBZBUATHHR43X3UK4B4L1",
+            client_secret: "RUSMZTKVV3TABPCOC1NTH532R2DA4PQTON2S4HMELETILQD2",
+            v: 20181126
+        };
+        return Object.keys(keys)
+            .map(key => `${key}=${keys[key]}`)
+            .join("&");
+    }
+    static urlBuilder(urlParams) {
+        if(!urlParams) {
+            return "";
+        }
+        return Object.keys(urlParams)
+            .map(key => `${key}=${urlParams[key]}`)
+            .join("&");
+    }
+    static headers() {
+        return {
+            Accept: "application/json"
+        };
+    }
+    static simpleFetch(endPoint, method, urlParams) {
+        let requestData = {
+            method,
+            headers: Helper.headers()
+        };
+        return fetch(`${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(
+            urlParams
+        )}`
+        ).then(res => res.json());
+    }
+}
+
+export default class FoursquareAPI {
+    static search(urlParams) {
+        return Helper.simpleFetch("/venues/search", "GET", urlParams);
+    }
+    static getVenueDetails(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET");
+    }
+    static getVenuephotos(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
+    }
+}
